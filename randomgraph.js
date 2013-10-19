7229244d948ef7c242f327bfcb53713ac0b49086
Gregor Aisch <contact@vis4.net>
2013-10-19 02:25:23 +0200

    added WattsStrogatz alpha

--- randomgraph.js
+++ randomgraph.js
@@ -88,42 +88,122 @@
         },
 
 
-        /**
-         * Watts-Strogatz
-         *
-         * @param n number of nodes
-         * @param K mean degree (even integer)
-         * @param beta rewiring probability [0..1]
-         */
-        WattsStrogatz: function(n, K, beta) {
-            var graph = { nodes: [], edges: [] },
-                i, j, t, edge,
-                edge_lut = {};
-            K = K>>1; // divide by two
-            for (i = 0; i < n; i++) {
-                graph.nodes.push({ label: 'node '+i });
-                // create a latice ring structure
-                for (j = 1; j <= K; j++) {
-                    edge = { source: i, target: (i+j)%n };
+
+        WattsStrogatz: {
+
+
+            /**
+             * Watts-Strogatz Small World model Alpha
+             *
+             * @param n number of nodes
+             * @param K mean degree (even integer)
+             * @param alpha rewiring probability [0..1]
+             */
+            alpha: function(n, k, alpha) {
+                var graph = { nodes: [], edges: [] },
+                    i, j, edge,
+                    p = Math.pow(10, -10),
+                    ec = 0,
+                    edge_lut = {},
+                    ids = [],
+                    nk_half = n * k / 2,
+                    Rij, sumRij, r, pij;
+
+                for (i = 0; i < n; i++) {
+                    graph.nodes.push({ label: 'node '+i });
+                    // create a latice ring structure
+                    edge = { source: i, target: (i+1)%n };
                     edge_lut[edge.source+'-'+edge.target] = edge;
                     graph.edges.push(edge);
+                    ec++;
                 }
-            }
-            // rewiring of edges
-            for (i = 0; i < n; i++) {
-                for (j = 1; j <= K; j++) { // for every pair of nodes
-                    if (Math.random() <= beta) {
-                        do {
-                            t = Math.floor(Math.random() * (n-1));
-                        } while (t == i || edge_lut[i+'-'+t]);
-                        var j_ = (i+j)%n;
-                        edge_lut[i+'-'+j_].target = t; // rewire
-                        edge_lut[i+'-'+t] = edge_lut[i+'-'+j_];
-                        delete edge_lut[i+'-'+j_];
+                // Creating n * k / 2 edges
+                while (ec < nk_half) {
+                    for (i = 0; i < n; i++) {
+                        ids.push(i);
+                    }
+                    while (ec < nk_half && ids.length > 0) {
+                        i = ids.splice(Math.floor(Math.random()*ids.length), 1)[0];
+                        Rij = [];
+                        sumRij = 0;
+                        for (j = 0; j < n; j++) {
+                            Rij[j] = calculateRij(i, j);
+                            sumRij += Rij[j];
+                        }
+                        r = Math.random();
+                        pij = 0;
+                        for (j = 0; j < n; j++) {
+                            if (i != j) {
+                                pij += Rij[j] / sumRij;
+                                if (r <= pij) {
+                                    edge = { source: i, target: j };
+                                    graph.edges.push(edge);
+                                    ec++;
+                                    edge_lut[edge.source+'-'+edge.target] = edge;
+                                }
+                            }
+                        }
                     }
                 }
+
+                return graph;
+
+                function calculateRij(i, j) {
+                    if (i == j || edge_lut[i+'-'+j]) return 0;
+                    var mij = calculatemij(i, j);
+                    if (mij >= k) return 1;
+                    if (mij === 0) return p;
+                    return Math.pow(mij / k, alpha) * (1 - p) + p;
+                }
+
+                function calculatemij(i, j) {
+                    var mij = 0, l;
+                    for (l = 0; l < n; l++) {
+                        if (l != i && l != j && edge_lut[i+'-'+l] && edge_lut[j+'-'+l]) {
+                            mij++;
+                        }
+                    }
+                    return mij;
+                }
+            },
+
+            /**
+             * Watts-Strogatz Small World model Beta
+             *
+             * @param n number of nodes
+             * @param K mean degree (even integer)
+             * @param beta rewiring probability [0..1]
+             */
+            beta: function(n, K, beta) {
+                var graph = { nodes: [], edges: [] },
+                    i, j, t, edge,
+                    edge_lut = {};
+                K = K>>1; // divide by two
+                for (i = 0; i < n; i++) {
+                    graph.nodes.push({ label: 'node '+i });
+                    // create a latice ring structure
+                    for (j = 1; j <= K; j++) {
+                        edge = { source: i, target: (i+j)%n };
+                        edge_lut[edge.source+'-'+edge.target] = edge;
+                        graph.edges.push(edge);
+                    }
+                }
+                // rewiring of edges
+                for (i = 0; i < n; i++) {
+                    for (j = 1; j <= K; j++) { // for every pair of nodes
+                        if (Math.random() <= beta) {
+                            do {
+                                t = Math.floor(Math.random() * (n-1));
+                            } while (t == i || edge_lut[i+'-'+t]);
+                            var j_ = (i+j)%n;
+                            edge_lut[i+'-'+j_].target = t; // rewire
+                            edge_lut[i+'-'+t] = edge_lut[i+'-'+j_];
+                            delete edge_lut[i+'-'+j_];
+                        }
+                    }
+                }
+                return graph;
             }
-            return graph;
         },
 
         /**
